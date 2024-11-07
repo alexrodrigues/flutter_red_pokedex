1061d41b9c0f9e9f05978ce479abb0200920bb60
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2024-11-07 11:48:26 -0300

    creating the tab structure

--- lib/features/details/presentation/widgets/details_success.dart
+++ lib/features/details/presentation/widgets/details_success.dart
@@ -5,18 +5,40 @@ import 'package:red_pokedex/features/components/poke_card.dart';
 import 'package:red_pokedex/features/details/presentation/bloc/details_bloc.dart';
 import 'package:red_pokedex/theme/theme.dart';
 
-class DetailsSuccess extends StatelessWidget {
+class DetailsSuccess extends StatefulWidget {
   const DetailsSuccess({super.key, required this.pokemon});
-
   final Pokemon pokemon;
 
   @override
+  State<DetailsSuccess> createState() => _DetailsSuccessState();
+}
+
+class _DetailsSuccessState extends State<DetailsSuccess>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final pokemonColor = RedPokedexTypeColor.getPokemonColor(
+            widget.pokemon.types.first.type.name)
+        .primary;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:
-            RedPokedexTypeColor.getPokemonColor(pokemon.types.first.type.name)
-                .primary,
+        backgroundColor: pokemonColor,
         iconTheme: const IconThemeData(color: Colors.white),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
@@ -27,26 +49,44 @@ class DetailsSuccess extends StatelessWidget {
       ),
       body: Container(
         decoration: BoxDecoration(
-          color:
-              RedPokedexTypeColor.getPokemonColor(pokemon.types.first.type.name)
-                  .primary,
+          color: pokemonColor,
         ),
         child: Column(
           children: [
-            PokeCard(pokemon: pokemon),
+            PokeCard(pokemon: widget.pokemon),
             Expanded(
               child: Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 16.0
-                ),
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
                 color: Colors.transparent,
                 child: Card(
                   elevation: 16.0,
-                  child: Center(
-                    child: Text(
-                      "This is a card with content",
-                      style: TextStyle(fontSize: 16),
-                    ),
+                  child: Column(
+                    children: [
+                      TabBar(
+                        controller: _tabController,
+                        labelColor: pokemonColor,
+                        unselectedLabelColor: Colors.black,
+                        dividerColor: Colors.transparent,
+                        labelStyle: textTheme.labelLarge,
+                        unselectedLabelStyle: textTheme.labelLarge,
+                        indicatorColor: pokemonColor,
+                        tabs: const [
+                          Tab(text: 'About'),
+                          Tab(text: 'Stats'),
+                          Tab(text: 'Evolution'),
+                        ],
+                      ),
+                      Expanded(
+                        child: TabBarView(
+                          controller: _tabController,
+                          children: [
+                            Center(child: Text('About Content')),
+                            Center(child: Text('Stats Content')),
+                            Center(child: Text('Evolution Content')),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
